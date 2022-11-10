777727ba53c490eb302f8a448f6c578fc211544f
jmstaricco <juan.staricco@decemberlabs.com>
2022-11-09 22:30:26 -0300

    Updates

---
+++ precache-manifest.602ba04f1aa9ff1966064ff3fdac7380.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "29f9947b29daf6030aaf",
+    "url": "/ChikisWebApp/static/css/main.617fa030.chunk.css"
+  },
+  {
+    "revision": "29f9947b29daf6030aaf",
+    "url": "/ChikisWebApp/static/js/main.29f9947b.chunk.js"
+  },
+  {
+    "revision": "11cd1e69d28229995efc",
+    "url": "/ChikisWebApp/static/css/1.0a78e9e6.chunk.css"
+  },
+  {
+    "revision": "11cd1e69d28229995efc",
+    "url": "/ChikisWebApp/static/js/1.11cd1e69.chunk.js"
+  },
+  {
+    "revision": "9780199bdb416522dc60",
+    "url": "/ChikisWebApp/static/js/runtime~main.9780199b.js"
+  },
+  {
+    "revision": "46113cf09575d41103ef8475eed1b26f",
+    "url": "/ChikisWebApp/static/media/white-hand.46113cf0.png"
+  },
+  {
+    "revision": "6f4b20e84953ec714afa57488dd408b9",
+    "url": "/ChikisWebApp/static/media/rest.6f4b20e8.jpg"
+  },
+  {
+    "revision": "e815618b8167208a95a3add24c2079a7",
+    "url": "/ChikisWebApp/static/media/rest1.e815618b.jpg"
+  },
+  {
+    "revision": "607d8c5c1e2ec6c3842d4465ff2f9a20",
+    "url": "/ChikisWebApp/static/media/rest2.607d8c5c.jpg"
+  },
+  {
+    "revision": "69a5c3173e871bba5ccbb03f19047b0e",
+    "url": "/ChikisWebApp/static/media/rest3.69a5c317.jpg"
+  },
+  {
+    "revision": "08b712fabaf8b9cf2c537dccc9e887d4",
+    "url": "/ChikisWebApp/static/media/vlad.08b712fa.jpg"
+  },
+  {
+    "revision": "16ed31e3a057b75b15ac7e8a593ed0a3",
+    "url": "/ChikisWebApp/static/media/vlad-img.16ed31e3.jpg"
+  },
+  {
+    "revision": "38c7ac15cc429d67b46f7585c258b3b5",
+    "url": "/ChikisWebApp/static/media/green_burger.38c7ac15.jpg"
+  },
+  {
+    "revision": "90c43111d6e3f02b477e54a3342d78a1",
+    "url": "/ChikisWebApp/static/media/contact_us.90c43111.jpg"
+  },
+  {
+    "revision": "f11a6e5a5ae93f01015ccefc2cb29291",
+    "url": "/ChikisWebApp/static/media/birthday.f11a6e5a.jpg"
+  },
+  {
+    "revision": "32ee529ec6bcc4967e5335a7127d1131",
+    "url": "/ChikisWebApp/static/media/offerpic3.32ee529e.jpg"
+  },
+  {
+    "revision": "3b8d7c2407dd4d3f76bff5536321c40a",
+    "url": "/ChikisWebApp/static/media/offerpic.3b8d7c24.jpg"
+  },
+  {
+    "revision": "98a31e019b14f021c970188c87cff3f7",
+    "url": "/ChikisWebApp/static/media/offerpic2.98a31e01.jpeg"
+  },
+  {
+    "revision": "5baccd35a9fb9f7767a6feac7fdcdaba",
+    "url": "/ChikisWebApp/static/media/pizzaParty.5baccd35.jpg"
+  },
+  {
+    "revision": "0c2e4622fcc719c7b374cdafebd66289",
+    "url": "/ChikisWebApp/static/media/jay.0c2e4622.jpg"
+  },
+  {
+    "revision": "18d75ebc9d6bf5886d25dddde754adf0",
+    "url": "/ChikisWebApp/static/media/backPizza.18d75ebc.jpg"
+  },
+  {
+    "revision": "f2174f5fd28a77e49006252b4d40c798",
+    "url": "/ChikisWebApp/static/media/serve.f2174f5f.png"
+  },
+  {
+    "revision": "56cff607150157f60d312e21b61291b4",
+    "url": "/ChikisWebApp/static/media/cart.56cff607.jpg"
+  },
+  {
+    "revision": "d5ab2f42acc1ee66473332ae03dc3dd7",
+    "url": "/ChikisWebApp/static/media/offer.d5ab2f42.png"
+  },
+  {
+    "revision": "49f4476dad2fbf914daa3de9ba4979d0",
+    "url": "/ChikisWebApp/index.html"
+  }
+];
